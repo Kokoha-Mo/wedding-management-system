f4a662283573ca625341c89e8c949d2b041404d6
Kokoha-Mo <as86829@gmail.com>
2026-03-16 14:09:01 +0800

    feat:公司端頁面串接登出功能

--- src/main/java/com/wedding/wedding_management_system/config/SecurityConfig.java
+++ src/main/java/com/wedding/wedding_management_system/config/SecurityConfig.java
@@ -5,7 +5,6 @@ import com.wedding.wedding_management_system.repository.EmployeeRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
